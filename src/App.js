7af02c45cabd4393867573c219a4b05fbe58f774
Rafaella Carmo Menezes <42049163+menezesrafaella@users.noreply.github.com>
2021-12-02 10:46:19 -0300

    feat: :art: filter,map,array

--- src/App.js
+++ src/App.js
@@ -1,35 +1,116 @@
 import React from 'react';
+// --------------------------- Aula de JSX - 2 --------------------------------- 
+// const persona = {
+//   client: 'Mario',
+//   age: 31,
+//   shop: [
+//     { name: 'Notebook', price: 'R$ 2500'},
+//     { name: 'Frezzer', price: 'R$ 3000'},
+//     { name: 'Smartphone', price: 'R$ 1500'},
+//   ],
+//   situation: false,
+// }
+// --------------------------- Aula de JSX - 2 --------------------------------- 
 
-const persona = {
-  client: 'Mario',
-  age: 31,
-  shop: [
-    { name: 'Notebook', price: 'R$ 2500'},
-    { name: 'Frezzer', price: 'R$ 3000'},
-    { name: 'Smartphone', price: 'R$ 1500'},
-  ],
-  situation: false,
-}
+
+// --------------------------- Aula de JSX - Array --------------------------------- 
+// Mostre apenas produtos que forem mais caros que R$ 1500
+const produtos = [
+  {
+    id: 1,
+    nome: 'Smartphone',
+    preco: 'R$ 2000',
+    cores: ['#29d8d5', '#252a34', '#fc3766'],
+  },
+  {
+    id: 2,
+    nome: 'Notebook',
+    preco: 'R$ 3000',
+    cores: ['#ffd045', '#d4394b', '#f37c59'],
+  },
+  {
+    id: 3,
+    nome: 'Tablet',
+    preco: 'R$ 1500',
+    cores: ['#365069', '#47c1c8', '#f95786'],
+  },
+];
 
 
 
 const App = () => {
-  const data = persona;
+  const dados = produtos.filter((produto) =>
+    Number(produto.preco.replace('R$ ', '')) > 1500)
+  console.log(dados)
 
-  const total = data.shop.map(item => Number(item.price.replace('R$ ', ''))).reduce((a,b) => a + b);
-  
   return (
-  <>
-    <p>Nome: {data.client}</p>
-    <p>Idade: {data.age}</p>  
-    <p> Total gasto: { total }</p>
-    <p>Situação: <span style={{ color: data.situation ? 'green' : 'red'  }}>{data.situation ? 'Ativa' : 'Inativa'}</span> </p> 
-    { total > 5000 && <p>Voce está gastando muito</p> }
-  </> //React.Fragment
+    <section>
+      {dados.map(({ id, nome, preco, cores }) => (
+        <div key={id}>
+          <h1>{nome}</h1>
+          <p>Preço: {preco} </p>
+          <ul>
+            {cores.map(cor => (
+              <li key={cor} style={{ color: 'white', backgroundColor: cor }}>{cor}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+
+    </section>
   )
-}
+};
+
+// Uma solução:
+// produtos.filter((produto) =>
+//   Number(produto.preco.replace('R$ ', '')) > 1500
+// ).map(produto => (
+//   <div key={produto.id}>
+//     <h1>{produto.nome}</h1>
+//     <p>Preço: {produto.preco}</p>
+//     <ul>
+//       {produto.cores.map((cor) => (
+//         <li key={cor} style={{ color: 'white', backgroundColor: cor }}> {cor} </li>
+//         ))}
+//     </ul>
+//   </div>
+// ))}
+
+// --------------------------- Aula de JSX - Array --------------------------------- 
+
+// O JSX necessita de uma key única para cada elemento da Array
+// Ex: <li key="e1">Apple</li>
+// const filmes = ['Harry Potter - A pedra filosofal', 'Harry potter - A camara secreta', 'Harry potter - O prisioneiro de Azkaban']
+
+// return (
+//   <ul>
+//     {filmes.map(filme => {
+//       return <li key={filme}>{filme}</li>
+//     }) }
+//   </ul>
+// )
+
+// --------------------------- Aula de JSX - Array --------------------------------- 
+
+
+// --------------------------- Aula de JSX - 2 --------------------------------- 
+// const data = persona;
+
+// const total = data.shop.map(item => Number(item.price.replace('R$ ', ''))).reduce((a,b) => a + b);
+
+// return (
+// <>
+//   <p>Nome: {data.client}</p>
+//   <p>Idade: {data.age}</p>  
+//   <p> Total gasto: { total }</p>
+//   <p>Situação: <span style={{ color: data.situation ? 'green' : 'red'  }}>{data.situation ? 'Ativa' : 'Inativa'}</span> </p> 
+//   { total > 5000 && <p>Voce está gastando muito</p> }
+// </> //React.Fragment
+// )
 
 // JSX é um javascript estendido, dando a ele novas funcionalidades, e depois sao convertidos para react.
 // className -> class e htmlFor -> for (label)
+// --------------------------- Aula de JSX - 2 --------------------------------- 
+
 
 export default App;
