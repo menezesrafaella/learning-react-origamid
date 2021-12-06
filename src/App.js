968782eb62bb12097bb12a445fb1f93817e74b34
Rafaella Carmo Menezes <42049163+menezesrafaella@users.noreply.github.com>
2021-12-06 14:47:35 -0300

    feat: :art: exercise solution hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import React, { useState } from 'react';
-import ButtonModal from './ButtonModal';
-import Modal from './Modal';
+import Produto from './Produto';
+
+// Os links abaixo puxam dados de um produto em formato JSON
+// https://ranekapi.origamid.dev/json/api/produto/tablet
+// https://ranekapi.origamid.dev/json/api/produto/smartphone
+// https://ranekapi.origamid.dev/json/api/produto/notebook
+// Crie uma interface com 3 botões, um para cada produto.
+// Ao clicar no botão faça um fetch a api e mostre os dados do produto na tela.
+// Mostre apenas um produto por vez
+// Mostre a mensagem carregando... enquanto o fetch é realizado
 
 
 
 const App = () => {
-  // O primeiro valor guarda o dado do estado atual, pode ser qualquer tipo de dado como strings, arrays, números, boolean, null, undefined e objetos. O segundo valor é uma função que pode ser utilizada para modificarmos o estado do primeiro valor.
-  const [modal, setModal] = useState(false)
+  const [dados, setDados] = useState(null)
+  const [carregando, setCarregando] = useState(null)
+
+  async function handleClick(event) {
+
+    const response = await fetch(
+      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
+    );
+    const json = await response.json();
+    setDados(json);
+    setCarregando(false);
+  }
 
   return (
     <div>
-      <Modal modal={modal} setModal={setModal} />
-      <ButtonModal setModal={setModal} />
+      <button onClick={handleClick}>Tablet</button>
+      <button onClick={handleClick}>Smartphone</button>
+      <button onClick={handleClick}>Notebook</button>
+      {carregando && <p>Carregando...</p>}
+      {!carregando && dados && <Produto dados={dados} />}
     </div>
   )
 };
 
 export default App;
 
+// ------------------------ UseState ----------------------------------
+
+// // O primeiro valor guarda o dado do estado atual, pode ser qualquer tipo de dado como strings, arrays, números, boolean, null, undefined e objetos. O segundo valor é uma função que pode ser utilizada para modificarmos o estado do primeiro valor.
+// const [modal, setModal] = useState(false)
+
+// return (
+//   <div>
+//     <Modal modal={modal} setModal={setModal} />
+//     <ButtonModal setModal={setModal} />
+//   </div>
+// )
 {/* <button onClick={() => setAtivo(!ativo)}>{ativo ? 'Ativo' : 'Inativo'}</button> */ }
+
+// ------------------------ UseState ----------------------------------
 
 // Desafio. Primeira solução:
 // Replique a interface como a apresentada na aula
--- src/ButtonModal.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ButtonModal = ({ setModal }) => {
-  return (
-    <button onClick={() => setModal(true)}> Abrir Modal </button>
-  )
-}
-
-export default ButtonModal
