72311a2f3d816d096c96c395613f3dec17aafaf9
Rafaella Carmo Menezes <42049163+menezesrafaella@users.noreply.github.com>
2022-01-09 20:13:43 -0300

    feat: challenge form

--- src/Components/Radio.js
+++ src/Components/Radio.js
@@ -1,24 +1,33 @@
-import React from 'react'
+import React from 'react';
 
-const Radio = ({ options, value, setValue, ...props }) => {
-  const [radio, setRadio] = React.useState('');
-
+const Radio = ({ pergunta, options, onChange, value, id, active }) => {
+  if (active === false) return null;
   return (
-    <>
+    <fieldset
+      style={{
+        padding: '2rem',
+        marginBottom: '1rem',
+        border: '2px solid #eee',
+      }}
+    >
+      <legend style={{ fontWeight: 'bold' }}>{pergunta}</legend>
       {options.map((option) => (
-        <label key={option}>
+        <label
+          key={option}
+          style={{ marginBottom: '1rem', fontFamily: 'monospace' }}
+        >
           <input
             type="radio"
-            value={option}
+            id={id}
             checked={value === option}
-            onChange={({ target }) => setValue(target.value)}
-            {...props}
+            value={option}
+            onChange={onChange}
           />
           {option}
         </label>
       ))}
-    </>
-  )
-}
+    </fieldset>
+  );
+};
 
 export default Radio;
